5c54b07e2b9973f0dc276f14289c84cdf130d1b8
kenkenk13 <kenkenk13@gmail.com>
2015-02-03 22:46:11 -0600

    added some (very) rough outlines to code

--- Project1/src/edu/gatech/seclass/prj1/AvgSentenceLength.java
+++ Project1/src/edu/gatech/seclass/prj1/AvgSentenceLength.java
@@ -1,5 +1,10 @@
 package edu.gatech.seclass.prj1;
 
 public class AvgSentenceLength {
+	private Sentence[] Document = null;
+	
+	public AvgSentenceLength() {
+		
+	}
 
 }
--- Project1/src/edu/gatech/seclass/prj1/Main.java
+++ Project1/src/edu/gatech/seclass/prj1/Main.java
@@ -1,5 +1,23 @@
 package edu.gatech.seclass.prj1;
 
+import java.io.*;
+
 public class Main {
+	public static void main(String[] args) {
+		AvgSentenceLength asl = new AvgSentenceLength();
+		/*
+		 * FileInputStream inFile = new FileInputStream(args[0]);
+		 * int cnt = 0;
+		 * char letter;
+		 * String Doc = "";
+		 * while((i=inFile.read())!=-1) {
+		 * letter = (char)cnt;
+		 * Doc += letter;
+		 * 
+		 * asl.setDoc(Doc); // write setter to separate into sentences split(".|\\!|\\?)
+		 * 			have multiple of this to compensate for possible command line args (commas etc)
+		 * asl.findAvgSentenceLength();
+		 */
+	}
 
 }
