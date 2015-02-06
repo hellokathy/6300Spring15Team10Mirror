02a579d252384ff84fc0c53f69103393d00c9505
kenkenk13 <kenkenk13@gmail.com>
2015-02-05 19:44:17 -0600

    Added empty test class

---
+++ Project1/test/edu/gatech/seclass/prj1/AvgSentenceLengthTest.java
@@ -0,0 +1,14 @@
+package edu.gatech.seclass.prj1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AvgSentenceLengthTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
