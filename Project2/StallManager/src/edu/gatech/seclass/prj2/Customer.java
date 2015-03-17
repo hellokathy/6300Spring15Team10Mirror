eabb81e23b9ae18164808448d18a860f1ed48bce
KenKenk13 <kenkenk13@gmail.com>
2015-03-17 18:42:16 -0500

    added some code and classes

--- Project2/StallManager/src/edu/gatech/seclass/prj2/Customer.java
+++ Project2/StallManager/src/edu/gatech/seclass/prj2/Customer.java
@@ -1,23 +1,60 @@
 package edu.gatech.seclass.prj2;
 
+import java.util.ArrayList;
+
 public class Customer {
 
-	private String firstName;
-	private String lastName;
-	private String zip;
-	private String email;
-	private boolean isGold = false;
-	private double AmtSpentCalYr = 0;
-	private double cumulativeDiscount = 0;
-	private String customerID;
+	private String szFName;
+	private String szLName;
+	private String szZip;
+	private String szEmail;
+	private String szCustomerID;
+	private boolean bIsGold = false;
+	private double dblAmtSpentCalYr = 0;
+	private double dblCumulativeDiscount = 0;
+	private ArrayList<Transaction> aTransactions = null;
+	private ArrayList<CreditCard> aCreditCards = null;
 	
 	public Customer(String firstName, String lastName, String zip, String email, String customerID){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.zip = zip;
-		this.email = email;
-		this.customerID = customerID;
+		this.szFName = firstName;
+		this.szLName = lastName;
+		this.szZip = zip;
+		this.szEmail = email;
+		this.szCustomerID = customerID;
+	}
+	
+	public void AddTransaction(Transaction trans) {
+		if(this.aTransactions == null)
+			this.aTransactions = new ArrayList<Transaction>();
 		
+		this.aTransactions.add(trans);
+	}
+	
+	public void AddCard(CreditCard card) {
+		if(this.aCreditCards == null)
+			this.aCreditCards = new ArrayList<CreditCard>();
+		
+		this.aCreditCards.add(card);
+	}
+	
+	public void setFName(String firstname) {
+		this.szFName = firstname;
+	}
+	
+	public void setLName(String lastname) {
+		this.szLName = lastname;
+	}
+	
+	public void setZip(String zip) {
+		this.szZip = zip;
+	}
+	
+	public void setEmail(String email) {
+		this.szEmail = email;
+	}
+	
+	public void setCumDiscount(double amt) {
+		this.dblCumulativeDiscount = amt;
 	}
 	
 }
