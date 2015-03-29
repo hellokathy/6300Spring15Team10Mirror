b06d3dca800cdb7b9bb5c05b457c4341ea9b0943
Lance DeSimone <lancedesi@msn.com>
2015-03-29 16:22:29 -0700

    Utilized credit card scan and payment processing services

--- Project2/StallManager/src/com/example/stallmanager/AddTransaction.java
+++ Project2/StallManager/src/com/example/stallmanager/AddTransaction.java
@@ -1,6 +1,8 @@
 package com.example.stallmanager;
 
 import edu.gatech.seclass.prj2.DatabaseOperations;
+import edu.gatech.seclass.services.CreditCardService;
+import edu.gatech.seclass.services.PaymentService;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -13,7 +15,7 @@ import android.widget.Toast;
 
 public class AddTransaction extends Activity {
 	
-	Button Submit;
+	Button Submit; 
 	String date, amount;
 	public static String acct;
 	Context ctx = this;
@@ -23,25 +25,38 @@ public class AddTransaction extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_transaction);
 		
+	}
+	
+	public void submitNewTransactionPressed(View view){
+		//Get the information the user entered and create a new transaction
+		date = ((EditText)findViewById(R.id.dateText)).getText().toString();
+		amount = ((EditText)findViewById(R.id.amountText)).getText().toString();
 		
-		Submit = (Button) findViewById(R.id.addTransactionSubmitButton);
-		
-		Submit.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				
-				//Get the information the user entered and create a new customer
-				date = ((EditText)findViewById(R.id.dateText)).getText().toString();
-				amount = ((EditText)findViewById(R.id.amountText)).getText().toString();
-				
-				DatabaseOperations DB = new DatabaseOperations(ctx);
-				DB.EnterTransactionInfo(DB, amount, date, acct);
-				DB.close();
-				Toast.makeText(getBaseContext(), "Transaction added successfully", Toast.LENGTH_LONG).show();
-				Intent launchactivity = new Intent(ctx, MainActivity.class);
-				startActivity(launchactivity);
-				finish();
-			}
-		});
+		String scanCard = CreditCardService.getCardInfo();
+		if(scanCard == "Error"){
+			Toast.makeText(getBaseContext(), "Card scan failed, try again", Toast.LENGTH_LONG).show();
+		}
+		else{
+	    String [] toProcess = scanCard.split("#");
+	    boolean processSuccessful = PaymentService.processPayment(toProcess[0], toProcess[1], toProcess[2], toProcess[3], toProcess[4], Double.valueOf(amount));
+	    if(processSuccessful){
+	    //Credit card was processed successfully
+	    	
+	    //TODO: This is probably the point where we should figure out if the customer making a purchase
+	    //		is eligible for any discounts
+		DatabaseOperations DB = new DatabaseOperations(ctx);
+		DB.EnterTransactionInfo(DB, amount, date, acct);
+		DB.close();
+		Toast.makeText(getBaseContext(), "Transaction added successfully", Toast.LENGTH_LONG).show();
+		Intent launchactivity = new Intent(ctx, MainActivity.class);
+		startActivity(launchactivity);
+		finish();
+	    }
+	    else{
+	    //Payment processing failed
+		Toast.makeText(getBaseContext(), "Payment processing failed, try again", Toast.LENGTH_LONG).show();
+	    }
+		}
+
 	}
 }
