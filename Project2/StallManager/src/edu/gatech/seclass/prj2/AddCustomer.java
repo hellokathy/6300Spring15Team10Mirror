9af3a7e07a01e01dfd2189911751684bca344c98
Yanglei Zou <yangleizou@gatech.edu>
2015-04-04 01:58:46 -0500

    made AddCustomerClass more robust

--- Project2/StallManager/src/edu/gatech/seclass/prj2/AddCustomer.java
+++ Project2/StallManager/src/edu/gatech/seclass/prj2/AddCustomer.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,19 +15,18 @@ import android.widget.Toast;
 public class AddCustomer extends Activity {
 	Button Submit;
 	EditText Acct;
-	String firstName, lastName, zip, email, customerID;
 	Context ctx = this;
 	DatabaseOperations DB;
-
+	String firstName="", lastName="", zip="", email="", customerID;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_customer);
 		Acct = (EditText) findViewById(R.id.customerID);
-		Submit = (Button) findViewById(R.id.submitNewCustomerButton);
-
 		DB = new DatabaseOperations(ctx);
-		Cursor cr = DB.getLargestAcctnum(DB);
+		Cursor cr = DB.getLargestAcctnum(DB);	
+		
 		if( (cr.moveToLast()) ) {
 			int dex = cr.getColumnIndex(CustomerTableInfo.USER_ID);
 			String largestAcct = cr.getString(dex);
@@ -38,36 +36,45 @@ public class AddCustomer extends Activity {
 		else {
 			Acct.setText("1");
 		}
-
-		Submit.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
-				//Get the information the user entered and create a new customer
-				firstName = ((EditText)findViewById(R.id.firstNameText)).getText().toString();
-				lastName = ((EditText)findViewById(R.id.lastNameText)).getText().toString();
-				zip = ((EditText)findViewById(R.id.zipCodeText)).getText().toString();
-				email = ((EditText)findViewById(R.id.emailText)).getText().toString();
-				customerID = ((EditText)findViewById(R.id.customerID)).getText().toString();
-				
-				Cursor c = DB.getInfoByKey(DB, CustomerTableInfo.USER_ID, customerID);
-				if( !(c.moveToFirst()) ) {
-					DB.EnterCustomerInfo(DB, firstName, lastName, zip, email, customerID);
-					DB.close();
-					Toast.makeText(getBaseContext(), "Customer added successfully", Toast.LENGTH_LONG).show();
-					finish();
-				}
-				else {
-					Toast.makeText(getBaseContext(), "User ID already exists, please choose another", Toast.LENGTH_LONG).show();
-				}
-			}
-		});
 	}
 	
 	public void submitNewCustomerPressed(View view){
+		//Get the information the user entered and create a new customer
+		firstName = ((EditText)findViewById(R.id.firstNameText)).getText().toString();
+		lastName = ((EditText)findViewById(R.id.lastNameText)).getText().toString();
+		zip = ((EditText)findViewById(R.id.zipCodeText)).getText().toString();
+		email = ((EditText)findViewById(R.id.emailText)).getText().toString();
+		customerID = ((EditText)findViewById(R.id.customerID)).getText().toString();
+		
+		if(firstName.equals("") || lastName.equals("")){
+			Toast.makeText(getBaseContext(), "Please enter FULL NAME!", Toast.LENGTH_LONG).show();
+		}
+		else if(zip.equals("")){
+			Toast.makeText(getBaseContext(), "Please enter ZIP!", Toast.LENGTH_LONG).show();
+		}
+		else if(email.equals("")){
+			Toast.makeText(getBaseContext(), "Please enter EMAIL!", Toast.LENGTH_LONG).show();
+		}
+		else{
+			Cursor c = DB.getInfoByKey(DB, CustomerTableInfo.USER_ID, customerID);
+			if( !(c.moveToFirst()) ) {
+				DB.EnterCustomerInfo(DB, firstName, lastName, zip, email, customerID);
+				DB.close();
+				Toast.makeText(getBaseContext(), "Customer added successfully!", Toast.LENGTH_LONG).show();
+			}
+			else {
+				Toast.makeText(getBaseContext(), "User ID already exists, please choose another!", Toast.LENGTH_LONG).show();
+			}
+			//Switch back to the main view
+			Intent launchactivity= new Intent(AddCustomer.this, MainActivity.class);   
+			startActivity(launchactivity);  
+		}
+	}
+	
+	public void addCustomerReturnPressed(View view){		
 		//Switch back to the main view
-		setContentView(R.layout.activity_main);
-		Intent launchactivity= new Intent(AddCustomer.this, MainActivity.class);                             
-		startActivity(launchactivity);
+		Intent launchactivity= new Intent(AddCustomer.this, MainActivity.class);   
+		startActivity(launchactivity);       
 	}
 }
+
